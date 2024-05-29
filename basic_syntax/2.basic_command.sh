5ea27dc94923917247f81e45e836dd32aa3e5710
jiho3634 <jiho3634@gmail.com>
2024-05-29 15:32:43 +0900

    checkout test

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -24,7 +24,17 @@ git log --oneline
 # 전체 이력 조회
 git log --all
 
+# 로그를 그래프 형태로 조회
+git log --graph
+
 # 원격 저장소로 업로드
 git push origin 브랜치명
 
 # 충돌 무시하고 강제 push
+git push --force origin master
+
+# 특정 commit ID로 전환
+git checkout commit_ID
+
+# 다시 현재의 master(브랜치)의 commit로 return
+git checkout master
